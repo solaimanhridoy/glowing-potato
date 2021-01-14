afefbc28878ea5da632ade742217940123ebc912
lib10 <solaimanhridoy@gmail.com>
2021-01-14 20:39:57 +0600

    REMOVE Secret key

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v5*l84_g5=s&b1gu6%hruxpln@1ij0f3ydnuf*hx3@^htx@fu9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
