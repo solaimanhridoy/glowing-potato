07d138011a43072e8a12f324a0302271bf4b2391
solaimanhridoy <solaimanhridoy@gmail.com>
2021-11-13 18:57:15 +0600

    FIX run issue

--- blog/test_settings.py
+++ blog/test_settings.py
@@ -1,8 +1,9 @@
 from my_portfolio.settings import *
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "test_db.sqlite3"),
     }
 }
